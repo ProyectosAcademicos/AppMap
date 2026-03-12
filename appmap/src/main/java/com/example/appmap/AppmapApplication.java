ea5a8e01f84234779e725c744b6955e5a100c570
JonSaraviaZ <jona.saravia@duocuc.cl>
2026-03-11 21:06:19 -0300

    first commit

---
+++ appmap/src/main/java/com/example/appmap/AppmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.appmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmapApplication.class, args);
+	}
+
+}
